64746e5bca6e2eb2f64dad69b41c308145a43809
andiapri11 <andiapri1111@gmail.com>
2026-01-03 04:06:41 +0700

    Fix: Disable route caching to prevent crash

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,7 +21,7 @@ if [ "$APP_ENV" = "production" ]; then
         php artisan key:generate --show
     fi
     php artisan config:cache
-    php artisan route:cache
+    # php artisan route:cache
     php artisan view:cache
 fi
 
