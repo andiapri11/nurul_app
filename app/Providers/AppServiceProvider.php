2347e2223f55f6af1289e4aff291c7069a3e039f
andiapri11 <andiapri1111@gmail.com>
2026-01-03 04:49:06 +0700

    Fix: Force HTTPS for secure login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        if (config('app.env') === 'production') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
